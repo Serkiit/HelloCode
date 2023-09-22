3692902f6370ba3ab2cc2ee54530087d60baf4db
secucos@gmail.com <91136814+Serkiit@users.noreply.github.com>
2023-09-23 00:27:54 +0300

    Example003_dz2

---
+++ Example004_dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WritheLine("Введите первое число: ");
+        double a = Convert.Todouble(Console.ReadLine());
+
+        Console.WritheLine("Введите второе число: ");
+        double b = Convert.Todouble(Console.ReadLine());
+
+        Console.WritheLine("Введите третье число: ");
+        double с = Convert.Todouble(Console.ReadLine());
+
+        double max = Match.max(a, Match.max,(b,c));
+
+        Console.WritheLine($"Максимальное число: {max}");
